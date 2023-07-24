322167c6b2461cfff5e71bef75703e1c635c0dde
Swati Sehgal <swsehgal@redhat.com>
2023-07-24 17:12:17 +0100

    cpu-nri-plugin: ensure cpus are allocated
    We test out that dummy cpusets are assigned to pods
    
    TODO: Figure out cpuquota updates needed
    
    Signed-off-by: Swati Sehgal <swsehgal@redhat.com>

--- cmd/cpu-nri-plugin/main.go
+++ cmd/cpu-nri-plugin/main.go
@@ -21,12 +21,20 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/containers/podman/v4/pkg/env"
 	"github.com/sirupsen/logrus"
+	"k8s.io/utils/cpuset"
 	"sigs.k8s.io/yaml"
 
 	"github.com/containerd/nri/pkg/api"
 	"github.com/containerd/nri/pkg/stub"
+)
+
+const (
+	milliCPUToCPU         = 1000
+	CPUPluginEnvVarPrefix = "CPU_"
 )
 
 type config struct {
@@ -85,7 +93,8 @@ func (p *plugin) RemovePodSandbox(_ context.Context, pod *api.PodSandbox) error 
 
 func (p *plugin) CreateContainer(_ context.Context, pod *api.PodSandbox, ctr *api.Container) (*api.ContainerAdjustment, []*api.ContainerUpdate, error) {
 	log.Infof("Creating container %s/%s/%s...", pod.GetNamespace(), pod.GetName(), ctr.GetName())
-
+	adjustment := &api.ContainerAdjustment{}
+	updates := []*api.ContainerUpdate{}
 	//
 	// This is the container creation request handler. Because the container
 	// has not been created yet, this is the lifecycle event which allows you
@@ -98,8 +107,58 @@ func (p *plugin) CreateContainer(_ context.Context, pod *api.PodSandbox, ctr *ap
 	// functions in pkg/api/update.go to see the available controls.
 	//
 
-	adjustment := &api.ContainerAdjustment{}
-	updates := []*api.ContainerUpdate{}
+	if !AreCPUsBackedByDRARequested(ctr, pod) {
+		return adjustment, updates, nil
+	}
+
+	uniqueName := getCtrUniqueName(pod, ctr)
+	cpus := cpuset.New(2, 3)
+	log.Infof("append mutual cpus to container %q", uniqueName)
+	err := setCPUs(ctr, &cpus, uniqueName)
+	if err != nil {
+		return adjustment, updates, fmt.Errorf("CreateContainer: setting CPUs failed: %w", err)
+	}
+
+	//Adding mutual cpus without increasing cpuQuota,
+	//might result with throttling the processes' threads
+	//if the threads that are running under the mutual cpus
+	//oversteps their boundaries, or the threads that are running
+	//under the reserved cpus consumes the cpuQuota (pretty common in dpdk/latency sensitive applications).
+	//Since we can't determine the cpuQuota for the mutual cpus
+	//and avoid throttling the process is critical, increasing the cpuQuota to the maximum is the best option.
+	// quota, err := calculateCFSQuota(ctr)
+	// if err != nil {
+	// 	return adjustment, updates, fmt.Errorf("failed to calculate CFS quota: %w", err)
+	// }
+
+	// parentCfsQuotaPath, err := cgroups.Adapter.GetCFSQuotaPath(pod.GetLinux().GetCgroupParent())
+	// if err != nil {
+	// 	return adjustment, updates, fmt.Errorf("failed to find parent cfs quota: %w", err)
+	// }
+
+	// ctrCfsQuotaPath, err := cgroups.Adapter.GetCrioContainerCFSQuotaPath(pod.GetLinux().GetCgroupParent(), ctr.GetId())
+	// if err != nil {
+	// 	return adjustment, updates, fmt.Errorf("failed to find parent cfs quota: %w", err)
+	// }
+
+	// log.Infof("inject hook to modify container's cgroups %q quota to: %d", ctrCfsQuotaPath, quota)
+	// hook := &api.Hook{
+	// 	Path: "/bin/bash",
+	// 	// Args: []string{
+	// 	// 	"/bin/bash",
+	// 	// 	"-c",
+	// 	// 	fmt.Sprintf("echo %d > %s && echo %d > %s", quota, parentCfsQuotaPath, quota, ctrCfsQuotaPath),
+	// 	// },
+	// }
+	// adjustment.Hooks = &api.Hooks{
+	// 	CreateRuntime: []*api.Hook{hook},
+	// }
+
+	adjustment.Linux = &api.LinuxContainerAdjustment{
+		Resources: ctr.Linux.GetResources(),
+	}
+
+	log.Infof("sending adjustment to runtime: %+v", adjustment)
 
 	return adjustment, updates, nil
 }
@@ -198,4 +257,44 @@ func main() {
 		log.Errorf("plugin exited (%v)", err)
 		os.Exit(1)
 	}
+}
+
+func getCtrUniqueName(pod *api.PodSandbox, ctr *api.Container) string {
+	return fmt.Sprintf("%s/%s/%s", pod.GetNamespace(), pod.GetName(), ctr.GetName())
+}
+
+func setCPUs(ctr *api.Container, cpus *cpuset.CPUSet, uniqueName string) error {
+	lspec := ctr.GetLinux()
+
+	ctrCpus := lspec.Resources.Cpu
+	ctrCpus.Cpus = (*cpus).String()
+	log.Infof("container %q cpus ids after applying cpus %q", uniqueName, ctrCpus.Cpus)
+	return nil
+}
+
+// Requested checks whether a given container is requesting CPUs backed by DRA plugin
+func AreCPUsBackedByDRARequested(ctr *api.Container, pod *api.PodSandbox) bool {
+	log.Infof("CPUsBackedByDRARequested called: Pod ID: %q CPU Id: %q", pod.Name, ctr.Name)
+
+	if strings.HasPrefix(pod.Namespace, "cpu-test") {
+
+		log.Infof("Passed namespapace prefix test: Pod ID: %q CPU Id: %q", pod.Name, ctr.Name)
+		log.Infof("pod: pod.Linux.PodResources %v", pod.Linux.PodResources)
+		log.Infof("pod: pod.GetLinux().Resources.GetUnified() %v", pod.GetLinux().Resources.GetUnified())
+		log.Infof("ctr: ctr.Linux.GetResources() %v", ctr.Linux.GetResources())
+
+		envs, err := env.ParseSlice(ctr.Env)
+		if err != nil {
+			log.Errorf("failed to parse environment variables for container: %q; err: %v", ctr.Name, err)
+			return false
+		}
+
+		for k, v := range envs {
+			log.Infof("container :%q Environment variable: key %q value: %q", ctr.Name, k, v)
+			return true
+		}
+
+		return true
+	}
+	return false
 }
--- go.mod
+++ go.mod
@@ -30,10 +30,11 @@ replace (
 
 require (
 	github.com/container-orchestrated-devices/container-device-interface v0.5.4
+	github.com/containers/podman/v4 v4.6.0
 	github.com/google/uuid v1.3.0
 	github.com/jaypipes/ghw v0.11.0
 	github.com/prometheus/client_golang v1.14.0
-	github.com/spf13/cobra v1.6.0
+	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.14.0
 	k8s.io/api v0.27.1
@@ -47,7 +48,7 @@ require (
 )
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
@@ -63,17 +64,16 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.1 // indirect
-	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jaypipes/pcidb v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -82,22 +82,20 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
+	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/onsi/ginkgo/v2 v2.9.7 // indirect
-	github.com/onsi/gomega v1.27.7 // indirect
-	github.com/opencontainers/runc v1.1.4 // indirect
-	github.com/opencontainers/runtime-spec v1.0.3-0.20220825212826-86290f6a00fb // indirect
-	github.com/opencontainers/runtime-tools v0.9.1-0.20221107090550-2e043c6bd626 // indirect
+	github.com/opencontainers/runc v1.1.7 // indirect
+	github.com/opencontainers/runtime-spec v1.1.0-rc.3 // indirect
+	github.com/opencontainers/runtime-tools v0.9.1-0.20230317050512-e931285f4b69 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
-	github.com/sirupsen/logrus v1.9.0
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -109,7 +107,7 @@ require (
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/oauth2 v0.9.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/term v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
